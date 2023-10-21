02b8c77f5f86dd4014ac164d3be91e195bdeb01a
Ensar <ishakoglu.ensar@gmail.com>
2023-10-21 02:02:26 +0200

    feat: adds expertise icon component to select and filter projects

---
+++ src/app/components/expertise/expertise.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Expertise } from 'app/services/portfolio/portfolio.service';
+import { Observable, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-expertise',
+  templateUrl: './expertise.component.html',
+  styleUrls: ['./expertise.component.scss']
+})
+export class ExpertiseComponent implements OnInit, OnDestroy {
+  @Input({required: true}) expertise!: Expertise;
+  @Input({required: true}) clearSelectedEvent!: Observable<void>;
+
+  @Output() expertiseClickedEvent = new EventEmitter<string>();
+
+  selected = false;
+  private clearSelectedSubscription!: Subscription;
+
+  ngOnInit(): void {
+    this.clearSelectedSubscription = this.clearSelectedEvent.subscribe(() => {
+      this.selected = false;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.clearSelectedSubscription.unsubscribe();
+  }
+
+  expertiseClicked(): void {
+    this.selected = !this.selected;
+    this.expertiseClickedEvent.emit(this.expertise.name); 
+  }
+
+}
