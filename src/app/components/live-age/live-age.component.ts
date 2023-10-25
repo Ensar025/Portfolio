a54b4d98db32df64c26d2541ad4b0e10793c7a2d
Ensar <ishakoglu.ensar@gmail.com>
2023-10-25 14:08:35 +0200

    refactor: takes leap years into account

--- src/app/components/live-age/live-age.component.ts
+++ src/app/components/live-age/live-age.component.ts
@@ -23,7 +23,7 @@ export class LiveAgeComponent implements OnInit {
   ngOnInit() {
     const today = new Date();
 
-    if (today.getMonth() + 1 === this.birthDate.getMonth() + 1 && today.getDate() === this.birthDate.getDate()) {
+    if (today.getMonth() === this.birthDate.getMonth() && today.getDate() === this.birthDate.getDate()) {
       this.birthday = true;
     }
 
@@ -33,15 +33,70 @@ export class LiveAgeComponent implements OnInit {
   }
 
   private getCurrentAge(): LiveAge {
-    const ageInMillis = new Date(Date.now() - this.birthDate.getTime());
+    const currentDate = new Date();
+
+    const birthYear = this.birthDate.getUTCFullYear();
+
+    // Leap years
+    const february = (birthYear % 4 === 0 && birthYear % 100 !== 0) || birthYear % 400 === 0 ? 29 : 28;
+    const daysInMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    let yearDiff = currentDate.getUTCFullYear() - birthYear;
+    let monthDiff = currentDate.getUTCMonth() - this.birthDate.getUTCMonth();
+    let dayDiff = currentDate.getUTCDate() - this.birthDate.getUTCDate();
+    let hourDiff = currentDate.getUTCHours() - this.birthDate.getUTCHours();
+    let minuteDiff = currentDate.getUTCMinutes() - this.birthDate.getUTCMinutes();
+    let secondDiff = currentDate.getUTCSeconds() - this.birthDate.getUTCSeconds();
+
+    if (secondDiff < 0) {
+      minuteDiff--;
+      secondDiff += 60
+    }
+
+    if (minuteDiff < 0) {
+      hourDiff--;
+      minuteDiff += 60;
+    }
+
+    if (hourDiff < 0) {
+      dayDiff--;
+      hourDiff += 24;
+    }
+
+    if (dayDiff < 0) {
+      monthDiff--;
+
+      if (monthDiff > 0) {
+        dayDiff += daysInMonth[monthDiff];
+      }
+    }
+
+    if (monthDiff < 0) {
+      yearDiff--;
+      monthDiff += 12;
+      dayDiff += daysInMonth[monthDiff];
+    }
 
     return {
-      years: ageInMillis.getUTCFullYear() - 1970,
-      months: ageInMillis.getUTCMonth(),
-      days: ageInMillis.getUTCDate() - 1,
-      hours: ageInMillis.getUTCHours(),
-      minutes: ageInMillis.getUTCMinutes(),
-      seconds: ageInMillis.getUTCSeconds()
-  };
+      years: yearDiff,
+      months: monthDiff,
+      days: dayDiff,
+      hours: hourDiff,
+      minutes: minuteDiff,
+      seconds: secondDiff
+    };
+
+  //   const ageInMillis = new Date(Date.now() - this.birthDate.getTime());
+
+  //   return {
+  //     years: ageInMillis.getUTCFullYear() - 1970,
+  //     months: ageInMillis.getUTCMonth(),
+  //     days: ageInMillis.getUTCDate(),
+  //     hours: ageInMillis.getUTCHours(),
+  //     minutes: ageInMillis.getUTCMinutes(),
+  //     seconds: ageInMillis.getUTCSeconds()
+  // };
+
+  
   }
 }
